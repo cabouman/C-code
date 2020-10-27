e888c9f6ee6bd62aec9806f438ff2854ba715b5c
Charles Bouman <Charles.Bouman@gmail.com>
2020-10-27 16:54:19 -0400

    update

--- src/BtildeExample.c
+++ src/BtildeExample.c
@@ -8,24 +8,30 @@
 
 int main (int argc, char **argv) 
 {
-  double delta;  /* difference between pixel x_s and its neighbor x_r */
-  double b;       /* neighborhood relationship coefficient. */
-  double sigma_x; /* scale parameter. Increasing sigma_x reduces regularization. */
-  double p;       /* 1st shape parameter of qggmrf. Typically, p=1.2, and it is best to choose 1.0<p<=2.0 */
-  double q;       /* 2nd shape parameter of qggmrf. It is best to choose q=2.0 */
-  double T;       /* threshold parameter of qggmrf. Typically, T=1.0 */
-  double btilde;  /* Output value of btilde */
+  double delta;       /* difference between pixel x_s and its neighbor x_r */
+  double delta_prime; /* initial difference between pixel x_s^\prime and its neighbor x_r^\prime */
+  double b;           /* neighborhood relationship coefficient. */
+  double sigma_x;     /* scale parameter. Increasing sigma_x reduces regularization. */
+  double p;           /* 1st shape parameter of qggmrf. Typically, p=1.2, and it is best to choose 1.0<p<=2.0 */
+  double q;           /* 2nd shape parameter of qggmrf. It is best to choose q=2.0 */
+  double T;           /* threshold parameter of qggmrf. Typically, T=1.0 */
+  double btilde;      /* Output value of btilde */
+  double rho, rho_prime;
 
   delta = 1.0;
+
+  delta_prime = 1.0;
   b = 1.0;
   sigma_x = 1.0;
   p = 1.2;
   q = 2.0;
   T = 1.0;
 
-  btilde = get_btilde(delta, b, sigma_x, p, q, T);
+  btilde = get_btilde(delta_prime, b, sigma_x, p, q, T);
 
-  fprintf ( stdout, "btilde = %g\n", btilde );
+  rho_prime = get_rho(delta_prime, b, sigma_x, p, q, T);
+
+  fprintf ( stdout, "btilde = %g; rho_prime = %g\n", btilde , rho_prime );
 
   return(0);
 }
--- src/qGGMRF.c
+++ src/qGGMRF.c
@@ -1,28 +1,28 @@
 #include "qGGMRF.h"
 
 double get_btilde(
-    double delta,   /* difference between pixel x_s and its neighbor x_r */
-    double b,       /* neighborhood relationship coefficient. */
-    double sigma_x, /* scale parameter. Increasing sigma_x reduces regularization. */
-    double p,       /* 1st shape parameter of qggmrf. Typically, p=1.2, and it is best to choose 1.0<p<=2.0 */
-    double q,       /* 2nd shape parameter of qggmrf. It is best to choose q=2.0 */
-    double T        /* threshold parameter of qggmrf. Typically, T=1.0 */
+    double delta_prime, /* initial difference between pixel x_s^\prime and its neighbor x_r^\prime */
+    double b,       	/* neighborhood relationship coefficient. */
+    double sigma_x, 	/* scale parameter. Increasing sigma_x reduces regularization. */
+    double p,       	/* 1st shape parameter of qggmrf. Typically, p=1.2, and it is best to choose 1.0<p<=2.0 */
+    double q,       	/* 2nd shape parameter of qggmrf. It is best to choose q=2.0 */
+    double T        	/* threshold parameter of qggmrf. Typically, T=1.0 */
     ) 
 /* Computes the b_tilde parameter used for surrogate function minimization for the qGGMRF prior. */
 {
     double b_tilde;
     double tmpa, tmpb, tmpc; /* temporary constants used in calculation */
     
-    delta = fabs(delta) + sigma_x*DBL_EPSILON;
+    delta_prime = fabs(delta_prime) + sigma_x*DBL_EPSILON; /* this is for numberical stability */
 
     /* tmpa is the product of the first three terms reorganized for numberical stability when q=0. */ 
-    tmpa = b*pow(delta,q-2.0)/(2.0*pow(sigma_x,p)*pow(T*sigma_x,q-p));
+    tmpa = b*pow(delta_prime,q-2.0)/(2.0*pow(sigma_x,p)*pow(T*sigma_x,q-p));
 
     /* tmpc is the third term in formula. */ 
-    tmpb = pow(delta/(T*sigma_x),q-p);
+    tmpb = pow(delta_prime/(T*sigma_x),q-p);
     tmpc = ((q/p)+tmpb)/pow(1+tmpb,2.0);
 
-    b_tilde = tmpa*tmpb;    
+    b_tilde = tmpa*tmpc;    
     return b_tilde;
 }
 
@@ -37,18 +37,18 @@ double get_rho(
     ) 
 /* Computes the potential function rho for the qGGMRF prior. */
 {
-    double b_tilde;
+    double rho;
     double tmpa, tmpb, tmpc; /* temporary constants used in calculation */
     
-    delta = fabs(delta); /* + sigma_x*DBL_EPSILON; */
+    delta = fabs(delta) + sigma_x*DBL_EPSILON; /* this is for numberical stability */
 
-    /* tmpa is the product of the first three terms reorganized for numberical stability when q=0. */ 
-    tmpa = b*pow(delta,q-2.0)/(2.0*pow(sigma_x,p)*pow(T*sigma_x,q-p));
+    /* tmpa is the first term */ 
+    tmpa = pow(delta/sigma_x,p)/p;
 
-    /* tmpc is the third term in formula. */ 
+    /* tmpc is the second term */ 
     tmpb = pow(delta/(T*sigma_x),q-p);
-    tmpc = ((q/p)+tmpb)/pow(1+tmpb,2.0);
+    tmpc = tmpb/(1+tmpb);
 
-    b_tilde = tmpa*tmpb;    
-    return b_tilde;
+    rho = tmpa*tmpc;    
+    return rho;
 }
